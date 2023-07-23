506f9eb49566ef2327307dfe4418385719de590c
OHany1999 <omar1999410@gmail.com>
2023-07-23 15:34:00 +0200

    Finished

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,105 +8,136 @@ import 'package:mytube/utils/services.dart';
 import '../utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MyTube'),
-        centerTitle: true,
+        leading: Icon(Icons.play_circle, size: 45),
+        title: Text('MyTube',style: TextStyle(fontWeight: FontWeight.bold),),
       ),
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.all(10.0),
-            height: 70,
-            child: FutureBuilder<ChannelInfo>(
-              future: Services.getChannelInfo(),
-              builder: (context,snapshotForChannelInfo){
-                if(snapshotForChannelInfo.connectionState == ConnectionState.waiting){
-                  return Center();
-                }
-                if(snapshotForChannelInfo.hasError){
-                  return Center(
-                    child: Column(
-                      children: [
-                        Text('Something went wrong'),
-                        TextButton(onPressed: (){}, child: Text('Try Again'),),
-                      ],
-                    ),
-                  );
-                }
-                return ListView.builder(
-                  itemBuilder:(context,index){
+          FutureBuilder<ChannelInfo>(
+            future: Services.getChannelInfo(),
+            builder: (context, snapshotForChannelInfo) {
+              if (snapshotForChannelInfo.connectionState ==
+                  ConnectionState.waiting) {
+                return Center();
+              }
+              if (snapshotForChannelInfo.hasError) {
+                return Center(
+                  child: Column(
+                    children: [
+                      Text('Something went wrong'),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('Try Again'),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                margin: EdgeInsets.all(20),
+
+                padding: EdgeInsets.all(10.0),
+                height: 85,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
                     return Column(
                       children: [
-                        Row(children: [
-                          CircleAvatar(
-                            backgroundImage: CachedNetworkImageProvider(
-                                snapshotForChannelInfo.data!.items[index].snippet.thumbnails.medium.url
-                            ),
-                          ),
-                          SizedBox(width: 10,),
-                          Text('${snapshotForChannelInfo.data!.items[index].snippet.title}'),
-                        ],),
+                        CircleAvatar(
+                          backgroundImage: CachedNetworkImageProvider(
+                              snapshotForChannelInfo.data!.items[index].snippet
+                                  .thumbnails.medium.url),
+                        ),
+                        SizedBox(
+                          height:8,
+                        ),
+                        Text(
+                            '${snapshotForChannelInfo.data!.items[index].snippet.title}',style: TextStyle(fontWeight: FontWeight.w500),),
                       ],
                     );
-                  } ,
-                  itemCount: snapshotForChannelInfo.data!.items.length,);
-              },
-            ),
+                  },
+                  itemCount: snapshotForChannelInfo.data!.items.length,
+                ),
+              );
+            },
           ),
-          Expanded(
-            child: FutureBuilder<VideosModel>(
-              future: Services.getVideosList(
-                  playlistId: PlayListId),
-              builder:(context,snapshotForVideoList){
-                if(snapshotForVideoList.connectionState == ConnectionState.waiting){
-                  return Center(child: CircularProgressIndicator());
-                }
-                if(snapshotForVideoList.hasError){
-                  return Center(
-                    child: Column(
-                      children: [
-                        Text('Something went wrong'),
-                        TextButton(onPressed: (){}, child: Text('Try Again'),),
-                      ],
-                    ),
-                  );
-                }
-                return ListView.builder(
-                    itemCount:snapshotForVideoList.data!.videos.length ,
-                    itemBuilder: (context,index){
-                      return Container(
-                        padding: EdgeInsets.all(10.0),
-                        child: InkWell(
-                          onTap: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>PlayerScreen(snapshotForVideoList.data!.videos[index])));
-                          },
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  CachedNetworkImage(
-                                    imageUrl: snapshotForVideoList.data!.videos[index].snippet.thumbnails.thumbnailsDefault.url,
-                          ),
-                                  SizedBox(width: 10,),
-                                  Flexible(child: Text(snapshotForVideoList.data!.videos[index].snippet.title)),
-                                ],
+          FutureBuilder<VideosModel>(
+            future: Services.getVideosList(playlistId: PlayListId),
+            builder: (context, snapshotForVideoList) {
+              if (snapshotForVideoList.connectionState ==
+                  ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (snapshotForVideoList.hasError) {
+                print('err:${snapshotForVideoList.error}');
+                return Center(
+                  child: Column(
+                    children: [
+                      Text('Something went wrong'),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('Try Again'),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: snapshotForVideoList.data!.videos.length,
+                  separatorBuilder: (context, index) => Container(
+                        height: 1,
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PlayerScreen(
+                                      snapshotForVideoList
+                                          .data!.videos[index])));
+                        },
+                        child: Row(
+                          children: [
+                            CachedNetworkImage(
+                              imageUrl: snapshotForVideoList.data!.videos[index]
+                                  .snippet.thumbnails.thumbnailsDefault.url,
+                              height: 100,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Flexible(
+                              child: Text(
+                                snapshotForVideoList
+                                    .data!.videos[index].snippet.title,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 3,
+                                style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),
                               ),
-                            ],
-                          ),
+                            ),
+
+                          ],
                         ),
-                      );
-                    });
-              },
-            ),
+                      ),
+                    );
+                  });
+            },
           ),
         ],
       ),
     );
   }
 }
-
-
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 const String APIKEY = 'AIzaSyBmdki8OtORESM3uta0Ig7qV-DpzoCi774';
  const String ChannelId = 'UCIV86P2bEtB7PIwr69o4Cpw';
  const String BaseUrl = 'youtube.googleapis.com';
-const String PlayListId = 'PLgZpx-dmQGBMCG8UMSC5FUDq43mKRVFpX';
+const String PlayListId = 'PLgZpx-dmQGBOslTGSq7U-AyjLnXE3sYmX';
 const String PageToken = '';
 
